d416f51ac0685b128b0d5fc5c41e7f5a4d85e2c0
Vlad Trishch <vlad.trishch@gmail.com>
2016-08-29 23:50:27 +0300

    Added slick, lightbox, floating letters and images overlays

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,7 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function(){ // Nav module
   var toggleNav = function(){
     $('.nav').toggleClass('active')
   }
   $('.nav-close-button').click(toggleNav)
   $('.nav-open-button').click(toggleNav)
+})
+
+$(document).ready(function(){ // Lightbox config module
+  lightbox.option({
+    maxWidth: $(window).width()/2.5
+  })
+})
+
+$(document).ready(function(){ // Slick config module
+  $('.slick-slider').slick({
+    autoPlaySpeed: 3000,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  })
+  .on('afterChange', function(slick, currentSlideObj){
+    var activeCheckbox = currentSlideObj.currentSlide < 3 ? 0 : 1
+    $('.slider-control-box.active').removeClass('active')
+    $('.slider-control-box:eq(' + activeCheckbox + ')').addClass('active')
+    $('.current-slide').html('0' + (activeCheckbox + 1))
+  })
+  $('.slider-control-box').click(function(){
+    var index = $(this).index()
+    $('.slick-slider').slick('slickGoTo', index ? 4 : 1)
+  })
 })
