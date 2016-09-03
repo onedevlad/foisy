9b297089b833e0db87008aa890a6c2417ffb4e4e
Vlad Trishch <vlad.trishch@gmail.com>
2016-09-03 21:16:59 +0300

    UI fixes

--- scripts/blog.js
+++ scripts/blog.js
@@ -5,3 +5,9 @@ $(document).ready(function(){ // Nav module
   $('.nav-close-button').click(toggleNav)
   $('.nav-open-button').click(toggleNav)
 })
+
+$(document).ready(function(){ // Lightbox config module
+  lightbox.option({
+    maxWidth: $(window).width()/2.5
+  })
+})
