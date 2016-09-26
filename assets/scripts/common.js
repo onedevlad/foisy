fc9936bf7f6fcd6b520c84e996b32b032c58dfb5
Vlad Trishch <vlad.trishch@gmail.com>
2016-09-26 21:03:28 +0300

    Finished magazine

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,12 +1,25 @@
 var headerSliderTimeout = 5000
 var scrollDownSpeed = 400
 var lightboxMaxWidth = 2.5
-var SliderAutoPlay = true
-var SliderAutoPlaySpeed = 5000
-
+var sliderConfig = {
+  autoPlaySpeed: true,
+  autoplay: 500,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: false,
+  responsive: [{
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true,
+    },
+  }],
+}
 
 // DO NOT EDIT BELOW THIS POINT
-
 
 $(document).ready(function(){ // Nav module
   var openNav = function() {
@@ -45,31 +58,26 @@ $(document).ready(function() { // Lightbox config module
 
 $(document).ready(function() { // Slick config module
   if(jQuery().slick) {
-    $('.slick-slider').slick({
-      autoPlaySpeed: SliderAutoPlaySpeed,
-      autoplay: SliderAutoPlay,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      responsive: [{
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      }],
-    })
+    $('.slick-slider').slick(sliderConfig)
     .on('afterChange', function(slick, currentSlideObj){
-      var activeCheckbox = currentSlideObj.currentSlide < 3 ? 0 : 1
+      if(currentSlideObj.currentSlide >= 0) activeCheckbox = 0
+      if(currentSlideObj.currentSlide >= 2) activeCheckbox = 1
+      if(currentSlideObj.currentSlide >= 5) activeCheckbox = 2
+      if(currentSlideObj.currentSlide >= 8) activeCheckbox = 3
       $('.slider-control-box.active').removeClass('active')
       $('.slider-control-box:eq(' + activeCheckbox + ')').addClass('active')
       $('.current-slide').html('0' + (activeCheckbox + 1))
     })
     $('.slider-control-box').click(function(){
       var index = $(this).index()
-      $('.slick-slider').slick('slickGoTo', index ? 4 : 1)
+      var indexToGo = 0
+      switch(index){
+        case 0: indexToGo = 0; break;
+        case 1: indexToGo = 4; break;
+        case 2: indexToGo = 7; break;
+        case 3: indexToGo = 10; break;
+      }
+      $('.slick-slider').slick('slickGoTo', indexToGo)
     })
   }
 })
