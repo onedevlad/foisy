8a91094c878f3ff40a700751e50ee245b3599379
Vlad Trishch <vlad.trishch@gmail.com>
2016-10-10 20:34:26 +0300

    Major bugs fixed (fixed styles including at home and slider rendering at magazine)

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -56,7 +56,7 @@ $(document).ready(function(){ // Nav module
 
 
 $(document).ready(function() { // Lightbox config module
-  lightbox.option(lightboxConfig)
+  if(window.lightbox) lightbox.option(lightboxConfig)
 })
 
 
